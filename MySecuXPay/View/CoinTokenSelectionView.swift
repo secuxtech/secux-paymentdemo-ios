8a52f984954a364657702523b7a3665f04c5f89f
maochunsun <maochuns.sun@gmail.com>
2020-03-26 18:10:58 +0800

    Update CoinTokenSelectionView.swift

--- MySecuXPay/View/CoinTokenSelectionView.swift
+++ MySecuXPay/View/CoinTokenSelectionView.swift
@@ -13,7 +13,7 @@ class CoinTokenSelectionView: UIView{
     lazy var itemImg: UIImageView = {
 
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "dct")
+        //imageView.image = UIImage(named: "dct")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(imageView)
         
@@ -31,7 +31,7 @@ class CoinTokenSelectionView: UIView{
         let label = UILabel()
 
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "DCT, SPC" //"\(row+1)"
+        label.text = "" //"\(row+1)"
 
         label.font = UIFont.init(name: UISetting.shared.boldFontName, size: 17)
         label.textColor = .white
@@ -73,6 +73,9 @@ class CoinTokenSelectionView: UIView{
         return imageView
     }()
     
+    var coin = ""
+    var token = ""
+    
     var dropdownMenuShows = false
     func dropdownToggle(){
         self.dropdownMenuShows = !self.dropdownMenuShows
@@ -81,26 +84,29 @@ class CoinTokenSelectionView: UIView{
             UIView.animate(withDuration: 0.5, animations: {
                 self.dropdownImg.transform = self.dropdownImg.transform.rotated(by: -(CGFloat.pi * 0.999))
                 
-                
-                
             })
         }else{
             UIView.animate(withDuration: 0.5, animations: {
-                self.dropdownImg.transform = self.dropdownImg.transform.rotated(by: CGFloat.pi)
+                self.dropdownImg.transform = self.dropdownImg.transform.rotated(by: CGFloat.pi * 0.999)
             })
         }
     }
     
     func setup(coin:String, token:String){
+        self.coin = coin
+        self.token = token
+        self.itemImg.image = CoinTokenAccount.getCoinLogo(type: coin)
         self.itemNameLabel.text = "\(coin), \(token)"
+        self.layer.borderColor = UIColor.white.cgColor
     }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
 
         self.backgroundColor = .clear
         self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderColor = UIColor.gray.cgColor
         
         let _ = self.itemImg
         let _ = self.itemNameLabel
@@ -112,7 +118,7 @@ class CoinTokenSelectionView: UIView{
         
         self.backgroundColor = .clear
         self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderColor = UIColor.gray.cgColor
         
         let _ = self.itemImg
         let _ = self.itemNameLabel
