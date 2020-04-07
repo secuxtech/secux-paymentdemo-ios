c66bbb005269a97ae61104b8a7c1812b69996c1f
maochunsun <maochuns.sun@gmail.com>
2020-04-07 22:27:25 +0800

    Create BridgeDSWallet.h

---
+++ MySecuXPay/BridgeHeader/BridgeDSWallet.h
@@ -0,0 +1,15 @@
+//
+//  BridgeDSWallet.h
+//  MySecuXPay
+//
+//  Created by Maochun Sun on 2020/4/7.
+//  Copyright © 2020 SecuX. All rights reserved.
+//
+
+#ifndef BridgeDSWallet_h
+#define BridgeDSWallet_h
+
+
+#endif /* BridgeDSWallet_h */
+
+#import <DashSync/DSWallet.h>
